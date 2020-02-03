4e497b10ad9fb48f805d6cdffe5e956b8903dba7
Felddy <markf@geekpad.com>
2020-02-03 12:17:47 -0500

    Add a setup.py file in attempt to get isort to work. It didn't, but this project is more like our other projects now.

---
+++ setup.py
@@ -0,0 +1,85 @@
+"""
+This is the setup module for the example project.
+
+Based on:
+
+- https://packaging.python.org/distributing/
+- https://github.com/pypa/sampleproject/blob/master/setup.py
+- https://blog.ionelmc.ro/2014/05/25/python-packaging/#the-structure
+"""
+
+# Third-Party Libraries
+from setuptools import setup
+
+
+def readme():
+    """Read in and return the contents of the project's README.md file."""
+    with open("README.md", encoding="utf-8") as f:
+        return f.read()
+
+
+def package_vars(version_file):
+    """Read in and return the variables defined by the version_file."""
+    pkg_vars = {}
+    with open(version_file) as f:
+        exec(f.read(), pkg_vars)  # nosec
+    return pkg_vars
+
+
+setup(
+    name="project_setup",
+    # Versions should comply with PEP440
+    version="1.0.0",
+    description="Documentation for Github projects in the cisagov organization.",
+    long_description=readme(),
+    long_description_content_type="text/markdown",
+    # NCATS "homepage"
+    url="https://www.us-cert.gov/resources/ncats",
+    # The project's main homepage
+    download_url="https://github.com/cisagov/skeleton-python-library",
+    # Author details
+    author="Cyber and Infrastructure Security Agency",
+    author_email="ncats@hq.dhs.gov",
+    license="License :: CC0 1.0 Universal (CC0 1.0) Public Domain Dedication",
+    # See https://pypi.python.org/pypi?%3Aaction=list_classifiers
+    classifiers=[
+        # How mature is this project? Common values are
+        #   3 - Alpha
+        #   4 - Beta
+        #   5 - Production/Stable
+        "Development Status :: 3 - Alpha",
+        # Indicate who your project is intended for
+        "Intended Audience :: Developers",
+        # Pick your license as you wish (should match "license" above)
+        "License :: CC0 1.0 Universal (CC0 1.0) Public Domain Dedication",
+        # Specify the Python versions you support here. In particular, ensure
+        # that you indicate whether you support Python 2, Python 3 or both.
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+    ],
+    python_requires=">=3.6",
+    # What does your project relate to?
+    keywords="documentation",
+    install_requires=["docopt", "setuptools >= 24.2.0", "schema", "PyGithub"],
+    extras_require={
+        "test": [
+            "pre-commit",
+            "coveralls",
+            # coveralls does not currently support coverage 5.0
+            # https://github.com/coveralls-clients/coveralls-python/issues/203
+            # is the issue for this on the coveralls project
+            "coverage < 5.0",
+            "pytest-cov",
+            "pytest",
+        ]
+    },
+    scripts=[
+        "project_setup/scripts/ansible-roles",
+        "project_setup/scripts/iam-to-travis",
+        "project_setup/scripts/skeleton",
+        "project_setup/scripts/ssm-param",
+    ],
+    entry_points={},
+)
